f44068acea9a35c8b081c44d4f6aa6e24e74fe97
Aldo Jara <aldo_psn@hotmail.com>
2026-07-29 14:15:28 -0500

    fix: configurar base path '/aldo-en-ruta' en astro.config.mjs para estilos y enlaces en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://aldojara.github.io',
+  base: '/aldo-en-ruta',
   output: 'static',
   build: {
     format: 'directory'
